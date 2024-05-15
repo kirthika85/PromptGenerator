63dc462b7c2bb34b8671b250dea71c1e3cafc7a9
kirthika85 <kirthika.rajamani@gmail.com>
2024-05-15 14:57:57 -0700

    Update prompt_generator.py

--- prompt_generator.py
+++ prompt_generator.py
@@ -1,18 +1,21 @@
 import streamlit as st
 from langchain.llms import OpenAI
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
 
-# Set up OpenAI API
-# OpenAI.api_key = "your-api-key"  # Replace with your OpenAI API key
+def generate_prompt(topic, genre):
+    prompt = f"Write a {genre} story about {topic}"
+    #response = openai.Completion.create(
+    #    engine="text-davinci-002",
+    #    prompt=prompt,
+    #     max_tokens=100)
+    #return response.choices[0].text.strip()
+    llm = ChatOpenAI(api_key=openai_api_key, model_name="gpt-3.5-turbo",temperature=0.5)
+    in_response = ChatPromptTemplate.from_template(prompt)
+    chain = in_response | llm
+    response = chain.invoke()
+    print(response)
 
-
-def generate_prompt(topic, genre, length):
-    prompt = f"Write a {genre} story about {topic}. It should be {length} words long."
-    response = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt=prompt,
-         max_tokens=100
-    )
-    return response.choices[0].text.strip()
 
 # Streamlit UI
 st.title("Creative Writing Prompt Generator")
@@ -21,7 +24,7 @@ openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
 # Input fields
 topic = st.text_input("Enter topic:")
 genre = st.selectbox("Select genre:", ["Science Fiction", "Fantasy", "Mystery", "Romance", "Thriller"])
-length = st.slider("Select prompt length (words):", min_value=50, max_value=200, step=10, value=100)
+#length = st.slider("Select prompt length (words):", min_value=50, max_value=200, step=10, value=100)
 generate_button = st.button("Generate Prompt")
 
      
@@ -35,7 +38,7 @@ if generate_button and openai_api_key.startswith('sk-'):
     if generate_button:
         if topic:
             st.write("Generating prompt...")
-            prompt = generate_prompt(topic, genre, length)
+            prompt = generate_prompt(topic, genre)
             st.write(f"**Prompt:** {prompt}")
         else:
             st.error("Please enter a topic.")
